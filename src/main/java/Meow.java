93f06af77553b73ed79a20bd66a2586a7eca2f2a
haoknowah <nrgman@tds.net>
2024-12-04 22:41:41 -0600

    functioning jar and launcher

---
+++ src/main/java/Meow.java
@@ -0,0 +1,30 @@
+
+
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import ui.Window;
+
+public class Meow {
+	public static void main(String[] args) throws IOException
+	{
+		//Tester tester = new Tester();
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				Window win;
+				try {
+					win = new Window();
+					win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
